aa1f0f0e6955884cf56485039cb8d3db45869974
Riddler <winhtutooheart2@gmail.com>
2026-04-21 00:48:14 +0900

    ok

--- music.py
+++ music.py
@@ -29,6 +29,7 @@ YDL_OPTIONS_FAST = {
     'cachedir': False,
     'lazy_extractors': True,
     'user-agent': 'Mozilla/5.0 (Linux; Android 13; SM-G991B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36',
+    'js_runtimes': {'node': {}},
 }
 
 YDL_OPTIONS_FALLBACK = {
